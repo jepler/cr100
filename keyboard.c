4f42cce933918e30ea455121c0f9e2a37a3ceb3c
Jeff Epler <jepler@gmail.com>
2024-10-11 08:23:30 -0500

    Don't spam so much when waiting for the keyboard

--- keyboard.c
+++ keyboard.c
@@ -63,8 +63,12 @@ bool keyboard_setup(PIO pio) {
     gpio_pull_down(KEYBOARD_DATA_PIN);
     gpio_pull_down(KEYBOARD_DATA_PIN + 1);
 
+    int i = 0, j = 1;
     while(!(gpio_get(KEYBOARD_DATA_PIN) && gpio_get(KEYBOARD_DATA_PIN + 1))) {
-        scrnprintf("Waiting for keyboard to boot...\r\n");
+        if(i ++ == j) {
+            scrnprintf("Waiting for keyboard to boot... %d ms so far\r", i);
+            j *= 10;
+        }
         sleep_ms(1);
     }
     
